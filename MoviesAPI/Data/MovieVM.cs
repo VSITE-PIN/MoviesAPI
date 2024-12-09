37f4d9d41737f676c00d7fb43442a68b7abc58a0
6ante9 <anpalic@vsite.hr>
2024-12-09 20:51:47 +0100

    Labos 4

---
+++ MoviesAPI/Data/MovieVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAPI.Data
+{
+	public class MovieVM
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Genre { get; set; }
+
+
+	}
+}
--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+using YourProjectNamespace.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<MoviesService>();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
