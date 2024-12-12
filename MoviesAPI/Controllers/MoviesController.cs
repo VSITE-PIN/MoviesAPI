45a2dbf0548b38ec3e9ec73a575be28929e65f58
Juled Halili <juhalili@vsite.hr>
2024-12-12 22:39:01 +0100

    Dodan dependency za MovieService

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data;
+using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    public class MoviesController : ControllerBase
+    {
+        public MovieService MovieService { get; set; }
+        public MoviesController(MovieService moviesService)
+        {
+            MovieService = moviesService;
+        }
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] Movie movie)
+        {
+            MovieService.AddMovie(movie);
+            return Ok();
+        }
+    }
+}
+
--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Controllers;
 using MoviesAPI.Data;
+using MoviesAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString") ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
+builder.Services.AddScoped<MoviesController>();
+builder.Services.AddScoped<MovieService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
