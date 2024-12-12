e5e209f6354a74ad12a34e5b8341d947221c9985
Davor Petrlić <dapetrli@vsite.hr>
2024-12-12 20:31:02 +0100

    Vjezba 4

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Services;
+using MoviesAPI.ViewModel;
+using System.Diagnostics.Metrics;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        public MoviesService _moviesService { get; set; }
+        public MoviesController(MoviesService moviesService)
+        {
+            _moviesService = moviesService;
+        }
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            var allBooks = _moviesService.GetAllMovies();
+            return Ok(allBooks);
+        }
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] MovieVM movie)
+        {
+            _moviesService.AddMovie(movie);
+            return Ok();    
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetBookById([FromQuery] int id)
+        {
+            var book = _moviesService.GetMovieById(id);
+            return Ok(book);
+        }
+        [HttpPut("id")]
+        public IActionResult UpdateMovieById([FromQuery] int id,
+        [FromBody] MovieVM movieVM)
+        {
+            var updatedMovie = _moviesService.UpdateMovieById(id, movieVM);
+            return Ok(updatedMovie);
+        }
+        [HttpDelete("id")]
+        public IActionResult DeleteMovie([FromQuery] int id)
+        {
+            _moviesService.DeleteMovie(id);
+            return Ok();
+        }
+    }
+}
--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+using MoviesAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<MoviesService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+ ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 
 var app = builder.Build();
 
