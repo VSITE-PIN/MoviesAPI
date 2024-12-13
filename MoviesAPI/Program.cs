f201283c6e9fe7c9a2e12072eda8b4791210d3c8
Dorijan Ivanković <doivanko@vsite.hr>
2024-12-13 19:47:58 +0100

    profesore ubit cu se jer neznam sta radim krivo

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,15 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+ ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<MoviesService>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ MoviesAPI/Services/MoviesService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+
+namespace MoviesAPI.Services
+{
+    public class MoviesService
+    {
+        private AppDbContext _context;
+        public MoviesService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddMovie(MovieVM book)
+        {
+            var newMovie = new Movie()
+            {
+                Name = Movie.Name,
+                Id = Movie.Id,
+                Year = Movie.Year,
+                Genre = Movie.Genre,
+                
+            };
+            _context.Movies.Add(newMovie);
+            _context.SaveChanges();
+        }
+
+
+        public List<Movie> GetAllMovies()
+        {
+            return _context.Movie.ToList();
+        }
+        public Movie GetMovieById(int id)
+        {
+            return _context.Movie.FirstOrDefault(x => x.Id == id);
+        }
+        public Movie UpdateMovieById(int id, MovieVM bookVM)
+        {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            if (movie != null)
+            {
+                movie.Name = MovieVM.Name;
+                movie.Id = MovieVM.Id;
+              
+                movie.Genre = MovieVM.Genre;
+                movie.Year = MovieVM.Year;
+            }
+            return movie;
+        }
+        public void DeleteMovie(int id)
+        {
+            var book = _context.Movies.FirstOrDefault(x => x.Id == id);
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
