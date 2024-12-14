2cb1259d07d2d3b0f5ced343cb0178ad2185691e
Bruno Barbarić <brbarbar@vsite.hr>
2024-12-14 22:24:42 +0100

    vj4

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Services;
+using MoviesAPI.Data;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MoviesService _moviesService;
+
+        public MovieController(MoviesService moviesService)
+        {
+            _moviesService = moviesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = await _moviesService.GetAllMoviesAsync();
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMovie(int id)
+        {
+            var movie = await _moviesService.GetMovieByIdAsync(id);
+            if (movie == null)
+                return NotFound();
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMovie([FromBody] Movie movie)
+        {
+            await _moviesService.AddMovieAsync(movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, [FromBody] Movie movie)
+        {
+            if (id != movie.Id)
+                return BadRequest();
+
+            await _moviesService.UpdateMovieAsync(movie);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            await _moviesService.DeleteMovieAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ MoviesAPI/Services/MoviesService.cs
@@ -0,0 +1,47 @@
+﻿using MoviesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Services
+{
+    public class MoviesService
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Movie>> GetAllMoviesAsync()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            return await _context.Movies.FindAsync(id);
+        }
+
+        public async Task AddMovieAsync(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMovieAsync(Movie movie)
+        {
+            _context.Movies.Update(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMovieAsync(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
